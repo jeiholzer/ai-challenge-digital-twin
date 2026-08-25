87f95a5469f77e4741359b8ad314faab6eb65152
jeiholzer <jeiholzer123@gmail.com>
2026-08-25 15:07:00 -0400

    Add files via upload
    Initial checkin

---
+++ app.py
@@ -0,0 +1,13 @@
+import gradio as gr
+
+def respond(message, history):
+	response = f"You said: {message}\n\
+		And I say what a beautiful day!"
+	return response
+
+gr.ChatInterface(
+	fn=respond,
+	title="Joel's first digital twin",
+	chatbot=gr.chatbot(),
+	description="Chat with my twin on line"
+).launch(server_name="0.0.0.0",server_port=int(os.environ.get("PORT",7860)))
